726931df1f97d7529ccf5de4040de367992f50de
OtsukaNoboru <mopinfish@gmail.com>
2018-12-08 17:54:43 +0900

    [mopinfish-cms] top ogp

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'We Love Sauna',
+    description: 'サウナ好きエンジニアがサウナ駆動開発で頑張るブログ',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
