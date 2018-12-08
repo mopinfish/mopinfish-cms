52e5f6f1fbf74c68a7db8151f519aec8dae12eff
OtsukaNoboru <mopinfish@gmail.com>
2018-12-08 17:32:00 +0900

    [mopinfish-cms] impl og metatags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -27,51 +27,47 @@ export const BlogPostTemplate = ({
               {title}
             </h1>
             <p>{description}</p>
-            <img src={'/img/' + image.relativePath} alt={image.relativePath}/>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
+          <img src={'/img/' + image.relativePath} alt={image.relativePath}/>
+          <PostContent content={content} />
+          {tags && tags.length ? (
+            <div style={{ marginTop: `4rem` }}>
+              <h4>Tags</h4>
+              <ul className="taglist">
+                {tags.map(tag => (
+                  <li key={tag + `tag`}>
+                    <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
         </div>
       </div>
-    </section>
-  )
+    </div>
+  </section>
+)
 }
 
 BlogPostTemplate.propTypes = {
-  content: PropTypes.node.isRequired,
-  contentComponent: PropTypes.func,
-  description: PropTypes.string,
-  title: PropTypes.string,
-  helmet: PropTypes.object,
+content: PropTypes.node.isRequired,
+contentComponent: PropTypes.func,
+description: PropTypes.string,
+title: PropTypes.string,
+helmet: PropTypes.object,
 }
 
 const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data
+const { markdownRemark: post } = data
 
-  return (
-    <Layout>
-      <BlogPostTemplate
+return (
+  <Layout>
+    <BlogPostTemplate
         content={post.html}
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
         helmet={
-          <Helmet
-            titleTemplate="%s | Blog"
-          >
-            <title>{`${post.frontmatter.title}`}</title>
-            <meta name="description" content={`${post.frontmatter.description}`} />
-          </Helmet>
+          <Meta post={post}>
+          </Meta>
         }
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
@@ -88,16 +84,14 @@ BlogPost.propTypes = {
 }
 
 const Meta = ({ post }) => {
-  const origin = 'https://shibe97.com'
-
   return (
     <Helmet
       title={`${post.frontmatter.title} | Blog`}
       meta={[
         { name: 'description', content: post.frontmatter.description },
-        { property: 'og:title', content: post.frontmatter.title },
+        { property: 'og:title', content: post.frontmatter.description },
         { property: 'og:description', content: post.frontmatter.description },
-        { property: 'og:image', content: `${origin}${post.frontmatter.image.relativePath}` },
+        { property: 'og:image', content: '/img/' + post.frontmatter.image.relativePath }
       ]}
     />
   )
